5f1983dd56c779637e0fe1d35179dbf23708c65b
Bruno Faria <nunes@outlook.com>
2017-10-03 10:43:30 +1100

    sample runbook

---
+++ RunBooks/SampleA/StackOverflow_LoginSample.ps1
@@ -0,0 +1,19 @@
+#Get the credential with the above name from the Automation Asset store
+$Cred = Get-AutomationPSCredential -Name "Automation"
+if(!$Cred) {
+    Throw "Could not find an Automation Credential Asset named '${CredentialAssetName}'. Make sure you have created one in this Automation Account."
+}
+
+$ClientSecret = ""
+$ApplicationId = ""
+$SubscriptionId = ""
+
+#New PSCredential Object
+$secpasswd = ConvertTo-SecureString $ClientSecret -AsPlainText -Force
+#$mycreds = New-Object System.Management.Automation.PSCredential ($ApplicationId , $secpasswd)
+
+#Login to subscription
+Login-AzureRmAccount -Credential $Cred -SubscriptionId $SubscriptionId
+
+#Get storage context
+New-AzureRmSqlDatabaseExport -ResourceGroupName "" -ServerName "" -DatabaseName "" -StorageKeyType "StorageAccessKey" -StorageKey "" -StorageUri "" -AdministratorLogin $ApplicationId -AdministratorLoginPassword $secpasswd -AuthenticationType "AdPassword"
